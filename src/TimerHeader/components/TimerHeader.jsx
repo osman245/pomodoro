c2e98537e9790533e034e0265c31eaa5e587e5db
sumraw9904 <osman245>
2021-01-09 22:22:30 -0500

    cooldude

--- src/TimerHeader/components/TimerHeader.jsx
+++ src/TimerHeader/components/TimerHeader.jsx
@@ -19,7 +19,7 @@ return (
 <div class="container">
 <div>
 <nav class="bg-white db dt-l w-100 border-box pa3 ph5-l ">
-  <a class="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" href="#" title="Home">
+  <a class="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l"  >
     <img  src="./logo_transparent.png" class="dib w2 h2 br-100" alt="Site Name"/>
   </a>
   <div class="db dtc-l v-mid w-100 w-75-l tc tr-l ">
